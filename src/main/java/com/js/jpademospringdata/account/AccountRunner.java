d5fca80529b7f2fc303fa57d1ccf6f60c81191ae
junseongday <junseongday@gmail.com>
2020-03-07 01:14:19 +0900

    하이버네이트, jpa기초 예제

---
+++ src/main/java/com/js/jpademospringdata/account/AccountRunner.java
@@ -0,0 +1,32 @@
+package com.js.jpademospringdata.account;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+public class AccountRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setUsername("JS2");
+        account.setPassword("jpa2");
+
+        //jpa
+//        entityManager.persist(account);
+
+        //hibernate
+        final Session session = entityManager.unwrap(Session.class);
+        session.save(account);
+    }
+}
